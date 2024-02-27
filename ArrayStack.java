24c22d2567396fd87d12b51ba88a0fdd798dc66c
rishisalvi <122333026+rishisalvi@users.noreply.github.com>
2024-02-27 09:58:05 -0800

    Add files via upload

---
+++ ArrayStack.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.EmptyStackException; 
+/**
+ * Simple Stack using ArrayList
+ * 
+ * @author Rishi Salvi
+ * @since February 27, 2024
+ */
+ public class ArrayStack<E> implements Stack<E>{
+	 private List<E> theStack; 
+	 
+	 public ArrayStack(){
+		 theStack = new ArrayList<E>();
+	 }
+	 
+	 /** @return		true if the stack is empty; false otherwise */
+	 public boolean isEmpty() {return theStack.isEmpty(); }
+	 
+	 /** @return		the top element on the stack */
+	 public E peek(){
+		 if (theStack.isEmpty())
+			throw new EmptyStackException(); 
+		return theStack.get(theStack.size() - 1); 
+	}
+	
+	/** @param obj		the object to put on the top of the stack */
+	public void push (E obj){ theStack.add(obj); }
+	
+	/** @return			the object removed from the top of the stack */
+	public E pop(){
+		if (theStack.isEmpty())
+			throw new EmptyStackException();
+		return theStack.remove(theStack.size() - 1); 
+	}
+ }
---
+++ SimpleCalc.java
@@ -0,0 +1,94 @@
+import java.util.List;		// used by expression evaluator
+
+/**
+ *	<Description goes here>
+ *
+ *	@author	
+ *	@since	
+ */
+public class SimpleCalc {
+	
+	private ExprUtils utils;	// expression utilities
+	
+	private ArrayStack<Double> valueStack;		// value stack
+	private ArrayStack<String> operatorStack;	// operator stack
+
+	// constructor	
+	public SimpleCalc() {
+		utils = new ExprUtils;
+		valueStack = new ArrayStack<Double>; 
+		operatorStack = new ArrayStack<String>; 
+	}
+	
+	public static void main(String[] args) {
+		SimpleCalc sc = new SimpleCalc();
+		sc.run();
+	}
+	
+	public void run() {
+		System.out.println("\nWelcome to SimpleCalc!!!");
+		runCalc();
+		System.out.println("\nThanks for using SimpleCalc! Goodbye.\n");
+	}
+	
+	/**
+	 *	Prompt the user for expressions, run the expression evaluator,
+	 *	and display the answer.
+	 */
+	public void runCalc() {
+		String expression = ""; 
+		while (!expression.equals("q")){
+			expression = Prompt.getString(" "); 
+			if (expression.equals("h"))
+				printHelp(); 
+			else if (!expression.equals("q"))
+				evaluateExpression(utils.tokenizeExpression(expression)); 
+		}
+	}
+	
+	/**	Print help */
+	public void printHelp() {
+		System.out.println("Help:");
+		System.out.println("  h - this message\n  q - quit\n");
+		System.out.println("Expressions can contain:");
+		System.out.println("  integers or decimal numbers");
+		System.out.println("  arithmetic operators +, -, *, /, %, ^");
+		System.out.println("  parentheses '(' and ')'");
+	}
+	
+	/**
+	 *	Evaluate expression and return the value
+	 *	@param tokens	a List of String tokens making up an arithmetic expression
+	 *	@return			a double value of the evaluated expression
+	 */
+	public double evaluateExpression(List<String> tokens) {
+		double value = 0;
+		for (int i = 0; i < tokens.size; i++){
+			String token = tokens.get(token); 
+			
+		}
+		return value;
+	}
+	
+	/**
+	 *	Precedence of operators
+	 *	@param op1	operator 1
+	 *	@param op2	operator 2
+	 *	@return		true if op2 has higher or same precedence as op1; false otherwise
+	 *	Algorithm:
+	 *		if op1 is exponent, then false
+	 *		if op2 is either left or right parenthesis, then false
+	 *		if op1 is multiplication or division or modulus and 
+	 *				op2 is addition or subtraction, then false
+	 *		otherwise true
+	 */
+	private boolean hasPrecedence(String op1, String op2) {
+		if (op1.equals("^")) return false;
+		if (op2.equals("(") || op2.equals(")")) return false;
+		if ((op1.equals("*") || op1.equals("/") || op1.equals("%")) 
+				&& (op2.equals("+") || op2.equals("-")))
+			return false;
+		return true;
+	}
+	 
+}
